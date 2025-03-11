19fea390aaa02d5bb302b6df19f3b748996df639
Ben Hays <benhays858@gmail.com>
2025-03-11 14:11:01 -0700

    Trying to add sprites using Pixi

---
+++ src/components/Benzo/Benzo.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Assets, Texture } from "pixi.js";
+import benzoImage from "../../assets/images/benzo.png"; // Adjust the path as needed
+
+export function Benzo({ parentRef }) {
+  // The Pixi.js `Sprite`
+  const spriteRef = useRef(null);
+
+  console.log("parentRef", parentRef);
+  // console.log("spriteRef", spriteRef);
+
+  const [texture, setTexture] = useState(Texture.EMPTY);
+  const [isActive, setIsActive] = useState(false);
+
+  // Preload the sprite if it hasn't been loaded yet
+  useEffect(() => {
+    if (parentRef) {
+      if (texture === Texture.EMPTY) {
+        console.log("kaboom");
+        console.log(
+          "parentRef.current.offsetWidth:",
+          parentRef.current.offsetWidth
+        );
+        console.log(
+          "parentRef.current.offsetHeight:",
+          parentRef.current.offsetHeight
+        );
+        Assets.load(benzoImage).then((result) => {
+          console.log("result", result);
+          setTexture(result);
+        });
+      }
+    }
+  }, [parentRef, texture]);
+
+  return (
+    <pixiSprite
+      ref={spriteRef}
+      anchor={0.5}
+      eventMode={"static"}
+      onClick={(event) => setIsActive(!isActive)}
+      scale={0.4}
+      texture={texture}
+      x={parentRef.current.offsetWidth / 2}
+      y={parentRef.current.offsetHeight / 2}
+    />
+  );
+}
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import "./hero.scss";
+import React, { useRef } from "react";
+import { Application, extend } from "@pixi/react";
+import { Container, Graphics, Sprite } from "pixi.js";
+import { Benzo } from "components/Benzo/Benzo";
 import Vignette from "components/Vignette/Vignette";
+import "./hero.scss";
+
+// extend tells @pixi/react what Pixi.js components are available
+extend({
+  Container,
+  Graphics,
+  Sprite,
+});
 
 export default function Hero() {
+  const parentRef = useRef(null);
+
   return (
     <div className="hero">
-      <div className="container">
+      <div className="container" ref={parentRef}>
         <Vignette />
-        <div className="content">Hero</div>
+        <div className="content">
+          <Application background="#ffff00" resizeTo={parentRef}>
+            <Benzo parentRef={parentRef} />
+          </Application>
+        </div>
       </div>
     </div>
   );
