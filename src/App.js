6d0bcfc338f3fb3d647e122fbddb95e50b5a5ea6
Ben Hays <benhays858@gmail.com>
2025-03-20 12:24:34 -0700

    Refactored the Brands component to improve the layout and animation of the logos.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.scss";
 import Brands from "components/Brands/Brands";
 import Hero from "components/Hero/Hero";
 import Separator from "components/Separator/Separator";
+import Wallpaper from "components/Wallpaper/Wallpaper";
 
 function App() {
   return (
     <div className="App">
+      <Wallpaper />
       <Hero />
       <Separator />
       <Brands />
--- src/components/Brands/components/Logos/Logos.jsx
+++ src/components/Brands/components/Logos/Logos.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import gsap from "gsap";
 import logos from "../../logos.js";
 
@@ -6,51 +6,42 @@ export default function Logos() {
   const quiltRef = useRef(null);
   const halfAmount = Math.ceil(logos.length / 2);
 
+  const setRowStyles = useCallback(
+    ({ radius, startingIndex, spacingOffset }) => {
+      const logos = quiltRef.current.children;
+      const arcAngle = Math.PI / 4;
+      const angleStep = arcAngle / (logos.length - 1);
+
+      for (let i = startingIndex; i < logos.length; i++) {
+        const angle = -arcAngle / 2 + (i - startingIndex) * angleStep * 2;
+        const x = radius * Math.sin(angle) * 2;
+        const y = radius * (1 - Math.cos(angle)) * 2 - spacingOffset;
+        logos[
+          i
+        ].style.transform = `translate(${x}px, ${y}px) rotate(${angle}rad) scale(0.8)`;
+      }
+
+      gsap.fromTo(
+        logos,
+        { opacity: 0 },
+        { opacity: 1, ease: "power2.in", stagger: 0.05, duration: 0.2 }
+      );
+    },
+    []
+  );
+
   useEffect(() => {
-    const logos = quiltRef.current.children;
-    const radius = 600; // Arc radius
-    const arcAngle = Math.PI / 4; // Angle of the arc (180° = Math.PI, 120° = Math.PI / 1.5, etc.)
-    const angleStep = arcAngle / (logos.length - 1);
-
-    for (let i = 0; i < halfAmount; i++) {
-      const angle = -arcAngle / 2 + i * angleStep * 2; // Center the arc
-      const x = radius * Math.sin(angle) * 2; // Horizontal position
-      const y = radius * (1 - Math.cos(angle)) * 2 - 70; // Vertical position to simulate an arc
-
-      logos[
-        i
-      ].style.transform = `translate(${x}px, ${y}px) rotate(${angle}rad) scale(0.8)`;
-    }
-
-    gsap.fromTo(
-      logos,
-      { opacity: 0 },
-      { opacity: 1, ease: "power2.in", stagger: 0.05, duration: 0.2 }
-    );
-  }, [halfAmount]);
-
-  useEffect(() => {
-    const logos = quiltRef.current.children;
-    const radius = 650; // Arc radius
-    const arcAngle = Math.PI / 4; // Angle of the arc (180° = Math.PI, 120° = Math.PI / 1.5, etc.)
-    const angleStep = arcAngle / (logos.length - 1);
-
-    for (let i = halfAmount; i < logos.length; i++) {
-      const angle = -arcAngle / 2 + (i - halfAmount) * angleStep * 2; // Center the arc
-      const x = radius * Math.sin(angle) * 2; // Horizontal position
-      const y = radius * (1 - Math.cos(angle)) * 2 + 70; // Vertical position to simulate an arc
-
-      logos[
-        i
-      ].style.transform = `translate(${x}px, ${y}px) rotate(${angle}rad) scale(0.8)`;
-    }
-
-    gsap.fromTo(
-      logos,
-      { opacity: 0 },
-      { opacity: 1, ease: "power2.in", stagger: 0.05, duration: 0.2 }
-    );
-  }, [halfAmount]);
+    setRowStyles({
+      radius: 600,
+      startingIndex: 0,
+      spacingOffset: 70,
+    });
+    setRowStyles({
+      radius: 650,
+      startingIndex: halfAmount,
+      spacingOffset: -70,
+    });
+  }, [setRowStyles, halfAmount]);
 
   return (
     <div className="brand-quilt" ref={quiltRef}>
