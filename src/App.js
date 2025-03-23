86cc743b5bd1495262d6fdd305f63bc5d448b79f
Ben Hays <benhays858@gmail.com>
2025-03-23 16:33:56 -0700

    Added PictureFrame component and styles, updated layout and hero styles, and added new images for the frame.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Brands from "components/sections/Brands/Brands";
 import Hell from "components/sections/Hell/Hell";
 import Hero from "components/sections/Hero/Hero";
 import Lighthouse from "components/sections/Lighthouse/Lighthouse";
+import PictureFrame from "components/ui/PictureFrame/PictureFrame";
 import Resume from "components/sections/Resume/Resume";
+import "./benzo-app.scss";
 
 function App() {
   return (
-    <main className="App">
+    <main className="benzo-app">
+      <PictureFrame />
       <Hero />
       <About />
       <Brands />
---
+++ src/components/ui/PictureFrame/PictureFrame.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.scss";
+
+export default function PictureFrame() {
+  return <section className="picture-frame"></section>;
+}
