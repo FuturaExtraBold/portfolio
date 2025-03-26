7507ec0de18408bd97e1d5a478fe1a5fa1b8264a
Ben Hays <benhays858@gmail.com>
2025-03-25 19:09:06 -0700

    Updated the Hell section

--- src/components/sections/Hell/Hell.jsx
+++ src/components/sections/Hell/Hell.jsx
@@ -1,22 +1,17 @@
 import React from "react";
+import SectionHeader from "components/ui/SectionHeader/SectionHeader";
 import imageHell from "../../../assets/images/hell/hell.jpg";
 import "./styles.scss";
 
 export default function Hell() {
   return (
     <section className="hell">
-      <div className="container hell__container">
-        <img className="hell__image" src={imageHell} alt="Hell" />
-      </div>
-      <div className="layout-test-specs">
-        Hell / Arbirtrary Content
-        <br />
-        1440 x 1440 (1:1 or Arbitrary)
-        <br />
-        Something cool before we descend into the depths of hell?
-        <br />
-        (Yes, the image needs to be fixed)
-      </div>
+      <img className="hell__image" src={imageHell} alt="Hell" />
+      <div className="overlay hell__overlay" />
+      <SectionHeader
+        subtitle="This is the end of the page. The fire looks cool though, right?"
+        title="Awww, Hell."
+      />
     </section>
   );
 }
