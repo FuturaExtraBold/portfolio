6b916e69d06c50c5061f7efbf78cb048b5d8faa1
Ben Hays <benhays858@gmail.com>
2025-04-04 16:20:14 -0700

    Got packages working with latest versions of react and pixi.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/first */
+import * as PIXI from "pixi.js";
+window.PIXI = PIXI; // 👈 Must happen before anything else imports @pixi/react
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppProvider } from "App/AppProvider";
