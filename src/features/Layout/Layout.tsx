5bd637f203c75b6c3700eacf847da5698e640335
Ben Hays <benhays858@gmail.com>
2025-05-27 13:47:23 -0700

    Got layout section working with responsive images

--- src/features/Layout/Layout.tsx
+++ src/features/Layout/Layout.tsx
@@ -1,7 +1,10 @@
 import { type JSX, useRef } from "react";
-import { Background, Container, Content, Section } from "layout";
+import { Container, Content, Section } from "layout";
 import { Separator } from "ui";
 import "./styles.scss";
+import imageDesktop from "./images/hero_layout_desktop.webp";
+import imageTablet from "./images/hero_layout_tablet.webp";
+import imageMobile from "./images/hero_layout_mobile.webp";
 
 export default function Layout(): JSX.Element {
   const parentRef = useRef<HTMLDivElement | null>(null);
@@ -9,11 +12,28 @@ export default function Layout(): JSX.Element {
   return (
     <Section className="layout">
       <Container className="layout__container" ref={parentRef as any}>
-        <Background className="layout__background">
-          <h1>Hello</h1>
-        </Background>
         <Content className="layout__content">
-          <h1>World!</h1>
+          <picture>
+            <source
+              srcSet={imageDesktop}
+              media="(min-width: 769px)"
+              width="2400"
+              height="1600"
+            />
+            <source
+              srcSet={imageTablet}
+              media="(min-width: 431px)"
+              width="1536"
+              height="1024"
+            />
+            <img
+              src={imageMobile}
+              alt="Layout example"
+              width="430"
+              height="1024"
+              className="layout__image"
+            />
+          </picture>
         </Content>
       </Container>
       <Separator />
