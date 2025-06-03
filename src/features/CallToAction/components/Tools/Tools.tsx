daae0fbc29626b3b6a0b63b537bfd97d61e77838
Ben Hays <benhays858@gmail.com>
2025-06-03 13:03:09 -0700

    Updated tools section

--- src/features/CallToAction/components/Tools/Tools.tsx
+++ src/features/CallToAction/components/Tools/Tools.tsx
@@ -1,65 +1,86 @@
 import {
-  FaReact,
-  FaNodeJs,
-  FaHtml5,
-  FaCss3Alt,
-  FaJs,
-  FaSass,
-  FaGithub,
+  FaChrome,
+  FaCss3,
   FaFigma,
-  // FaAdobe,
-  FaVimeoV,
-  FaSketch,
+  FaGithub,
+  FaHtml5,
+  FaImage,
+  FaJs,
+  FaNodeJs,
   FaNpm,
+  FaReact,
+  FaSass,
+  FaSketch,
 } from "react-icons/fa";
 import {
-  SiTypescript,
-  SiGatsby,
-  SiVite,
-  SiShopify,
-  // SiGsap,
-  // SiPixijs,
-  SiNetlify,
-  // SiVisualstudiocode,
-  SiExpress,
-  SiLighthouse,
-  SiAdobephotoshop,
   SiAdobeillustrator,
-  // SiActionscript,
-  // SiObjectivec,
+  SiAdobephotoshop,
+  SiAngular,
+  SiBootstrap,
+  SiContentful,
+  SiExpress,
+  SiGreensock,
+  SiLighthouse,
+  SiNetlify,
+  SiRuby,
+  SiRubyonrails,
+  SiShopify,
+  SiSvelte,
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+  SiVuedotjs,
 } from "react-icons/si";
+import { VscTerminal, VscVscode } from "react-icons/vsc";
+import { MdOutlineMarkEmailRead } from "react-icons/md";
 import "./styles.scss";
 
 export const Tools = () => {
   const skills = [
+    // Frameworks & Libraries
     { label: "React", icon: <FaReact /> },
     { label: "Typescript", icon: <SiTypescript /> },
     { label: "Javascript", icon: <FaJs /> },
-    { label: "HTML", icon: <FaHtml5 /> },
-    { label: "SCSS", icon: <FaSass /> },
+    { label: "Vite", icon: <SiVite /> },
+    { label: "Angular", icon: <SiAngular /> },
+    { label: "Svelte", icon: <SiSvelte /> },
+    { label: "Vue", icon: <SiVuedotjs /> },
+    { label: "GSAP", icon: <SiGreensock /> },
+    { label: "Pixi JS", icon: <FaJs /> },
+    { label: "Canvas", icon: <FaHtml5 /> },
+
+    // Design & UI Tools
     { label: "Photoshop", icon: <SiAdobephotoshop /> },
     { label: "Illustrator", icon: <SiAdobeillustrator /> },
     { label: "Figma", icon: <FaFigma /> },
     { label: "Sketch", icon: <FaSketch /> },
-    // { label: "VSCode", icon: <SiVisualstudiocode /> },
-    { label: "Liquid", icon: <SiShopify /> },
+    { label: "Bootstrap", icon: <SiBootstrap /> },
+    { label: "Tailwind", icon: <SiTailwindcss /> },
+    { label: "HTML", icon: <FaHtml5 /> },
+    { label: "CSS", icon: <FaCss3 /> },
+    { label: "Sass, SCSS", icon: <FaSass /> },
+
+    // Testing & Optimization
     { label: "Lighthouse", icon: <SiLighthouse /> },
-    { label: "ImageOptim", icon: <FaVimeoV /> }, // Placeholder icon
-    { label: "Clop", icon: <FaVimeoV /> }, // Placeholder icon
-    // { label: "Litmus", icon: <SiEmail /> }, // Placeholder icon
-    // { label: "Shopify", icon: <SiShopify /> },
+    { label: "Litmus", icon: <MdOutlineMarkEmailRead /> },
+    { label: "ImageOptim", icon: <FaImage /> },
+    { label: "Clop", icon: <FaImage /> },
+
+    // Dev Tools & Platforms
+    { label: "VSCode", icon: <VscVscode /> },
+    { label: "DevTools", icon: <FaChrome /> },
+    { label: "CLI, Terminal", icon: <VscTerminal /> },
     { label: "Github", icon: <FaGithub /> },
     { label: "Netlify", icon: <SiNetlify /> },
-    // { label: "GSAP", icon: <SiGsap /> },
-    { label: "Canvas", icon: <FaHtml5 /> },
-    { label: "Pixi JS", icon: <FaJs /> },
-    // { label: "Developer Tools", icon: <FaVimeoV /> }, // Placeholder icon
-    { label: "Express", icon: <SiExpress /> },
+    { label: "Contentful", icon: <SiContentful /> },
+
+    // Backend & Build
     { label: "Node", icon: <FaNodeJs /> },
+    { label: "Express", icon: <SiExpress /> },
     { label: "NPM", icon: <FaNpm /> },
-    { label: "Vite", icon: <SiVite /> },
-    // { label: "ActionScript", icon: <SiActionscript /> },
-    // { label: "Objective-C", icon: <SiObjectivec /> },
+    { label: "Liquid", icon: <SiShopify /> },
+    { label: "Ruby", icon: <SiRuby /> },
+    { label: "Rails", icon: <SiRubyonrails /> },
   ];
 
   return (
@@ -69,7 +90,7 @@ export const Tools = () => {
         {skills.map(({ label, icon }) => (
           <div key={label} className="text-light tools__item">
             <div className="tools__icon">{icon}</div>
-            <span className="tools__label">{label}</span>
+            <span className="body tools__label">{label}</span>
           </div>
         ))}
       </div>
