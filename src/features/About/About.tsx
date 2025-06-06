c64d25abb9e2811bf1a0a79f7103ad8f848fa5e0
Ben Hays <benhays858@gmail.com>
2025-06-06 13:23:10 -0700

    Switched to just text for About load check

--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -58,37 +58,35 @@ export default function About(): JSX.Element {
           <OverlayFade opacity={0.8} />
         </Background>
         <Content className="about__content">
+          <ResponsiveImage
+            alt="Ship"
+            className="about__image"
+            fallbackSrc={shipImage1x}
+            height={544}
+            hideOnMobile={true}
+            lazy={false}
+            ref={refShip}
+            sizes="(max-width: 768px) 248px, 496px"
+            srcSet={`${shipImage1x} 1x, ${shipImage2x} 2x`}
+            width={496}
+          />
           {showText && (
-            <>
-              <ResponsiveImage
-                alt="Ship"
-                className="about__image"
-                fallbackSrc={shipImage1x}
-                height={544}
-                hideOnMobile={true}
-                lazy={false}
-                ref={refShip}
-                sizes="(max-width: 768px) 248px, 496px"
-                srcSet={`${shipImage1x} 1x, ${shipImage2x} 2x`}
-                width={496}
-              />
-              <div className="about__text">
-                <h1 className="heading--2 about__title">
-                  Marvel at Feats of Spectacular Ingenuity
-                </h1>
-                <div className="body text-accent about__description">
-                  <span>
-                    A seasoned practitioner of the digital arts, I’ve spent over
-                    two decades conjuring responsive websites, immersive
-                    interfaces, and interactive experiences that delight and
-                    perform. With a sharp eye for detail and a touch of the
-                    theatrical, I specialize in elegant animations, refined
-                    design, and front-end sorcery powered by React, HTML, CSS,
-                    and their modern kin.
-                  </span>
-                </div>
+            <div className="about__text">
+              <h1 className="heading--2 about__title">
+                Marvel at Feats of Spectacular Ingenuity
+              </h1>
+              <div className="body text-accent about__description">
+                <span>
+                  A seasoned practitioner of the digital arts, I’ve spent over
+                  two decades conjuring responsive websites, immersive
+                  interfaces, and interactive experiences that delight and
+                  perform. With a sharp eye for detail and a touch of the
+                  theatrical, I specialize in elegant animations, refined
+                  design, and front-end sorcery powered by React, HTML, CSS, and
+                  their modern kin.
+                </span>
               </div>
-            </>
+            </div>
           )}
         </Content>
       </Container>
