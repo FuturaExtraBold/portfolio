bed25eab334640b086c8e2ec342e03b801d9bdb9
Ben Hays <benhays858@gmail.com>
2025-06-18 19:47:47 -0700

    Updated dates in the Resume section

--- src/features/Resume/components/Employers/Employers.tsx
+++ src/features/Resume/components/Employers/Employers.tsx
@@ -86,19 +86,19 @@ export default function Employers(): JSX.Element {
       <Employer
         logo={<Kajabi />}
         name="Kajabi"
-        tenure="2012 &ndash; 2024"
+        tenure="2013 &ndash; 2024"
         description="At Kajabi, our teams crafted intuitive tools that let creators share and sell what they know. From dynamic websites to seamless marketing, every element worked to fuel growth for our customer's business."
       />
       <Employer
         logo={<HookyGrand />}
         name="Hooky"
-        tenure="2008 &ndash; 2012"
+        tenure="2008 &ndash; 2013"
         description="At Hooky, our main philosophy was to play. By combining bold creative vision with unparalleled technical initiative, the small nimble project teams produced fresh and imaginative work for our clients, and their users."
       />
       <Employer
         logo={<Studio318 />}
         name="Studio318"
-        tenure="2006 &ndash; 2008"
+        tenure="2007 &ndash; 2008"
         description="At Studio 318, an award-winning, woman and minority-owned graphic design agency, we collaborated with industry leaders. Our passion for design and strategy turned concepts into lasting, compelling visuals."
       />
     </div>
