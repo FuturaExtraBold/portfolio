ffbf4707e2e806b6b8e18b4933f72ef2d52ffd12
Ben Hays <benhays858@gmail.com>
2026-03-09 15:49:18 -0700

    removed testing suite, as app is ephemeral and tests were not valuable.

--- src/layout/Guide/Guide.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Guide from "./Guide";
-
-describe("Guide component", () => {
-  it("renders children correctly", () => {
-    render(<Guide />);
-    expect(screen.getByTestId("guidelines")).toHaveClass("guidelines");
-  });
-});
